746ec76bfb2c7eadec991074a4a61355e2d4a205
daytonjallen <daytonjallen@gmail.com>
2022-09-17 08:28:02 -0700

    day1 part 1

---
+++ day1/day1.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+def countDepthIncreases(depths: List[int]) -> int:
+    """Counts the number of times a depth measurement increases.
+
+    Args:
+        depths (List[int]): depth measurements
+
+    Returns:
+        int: the number of times a depth measurement increases
+    """
+    if len(depths) <= 1:
+        return 0
+
+    numberOfIncreases = 0
+    lastDepth = depths[0]
+
+    for depth in depths[1:]:
+        if depth > lastDepth:
+            numberOfIncreases += 1
+        lastDepth = depth
+    return numberOfIncreases
+
+
+with open("./testcase.txt", "r") as test_file:
+    lines = test_file.readlines()
+    test_data = [int(line) for line in lines]
+
+result = countDepthIncreases(test_data)
+print(result)
