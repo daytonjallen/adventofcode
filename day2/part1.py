a56f35c427a2377d5c3f0edb08efab6ad5b4b580
daytonjallen <daytonjallen@gmail.com>
2022-09-18 08:26:29 -0700

    add day 2 solutions

---
+++ day2/part1.py
@@ -0,0 +1,35 @@
+from typing import List, Tuple
+
+
+def calculatePosition(commands: List[Tuple[str, int]]) -> int:
+    """Calculates submarines final position
+
+    Args:
+        commands (List[Tuple[str, int]]): command and value tuple
+
+    Returns:
+        int: final position
+    """
+    horizontalPosition = 0
+    depthPosition = 0
+
+    for command, value in commands:
+        if command == "forward":
+            horizontalPosition += value
+        elif command == "down":
+            depthPosition += value
+        else:
+            depthPosition -= value
+
+    return horizontalPosition * depthPosition
+
+
+with open("./testcase.txt", "r") as test_file:
+    lines = test_file.readlines()
+    test_data = [
+        (command, int(value))
+        for (command, value) in (line.split(" ") for line in lines)
+    ]
+
+result = calculatePosition(test_data)
+print(result)
